0d657b8b31f30ed7936bbc7c072f5aa3e4f8270f
Alek Dróżdż <Alek Drd>
2016-07-26 19:34:02 +0200

    background added

--- core/src/com/mygdx/hanoi/HanoiGame.java
+++ core/src/com/mygdx/hanoi/HanoiGame.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -12,6 +13,7 @@ public class HanoiGame extends ApplicationAdapter {
 	public final static String GAME_NAME = "Towers of Hanoi";
 	private SpriteBatch batch;
 	private BitmapFont font;
+	private Texture background;
 
 	
 	public void create() {
@@ -19,6 +21,7 @@ public class HanoiGame extends ApplicationAdapter {
 		font = new BitmapFont();
 		font.setColor(Color.BLACK);
 		font.getData().setScale(2.3f);
+		background = new Texture("background.png");
 	}
 
 	
@@ -26,6 +29,7 @@ public class HanoiGame extends ApplicationAdapter {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		batch.draw(background,0,0);
 		font.draw(batch, GAME_NAME, 280,440);
 		batch.end();
 	}
