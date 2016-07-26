81751cf6551d6842a81677c4043eb4783d7f3bf3
Alek Dróżdż <Alek Drd>
2016-07-26 18:10:28 +0200

    init

---
+++ desktop/src/com/mygdx/hanoi/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.hanoi.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.hanoi.HanoiGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new HanoiGame(), config);
+	}
+}
