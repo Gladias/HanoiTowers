6eb4adf4d0cc4ca262f1f5ed9c8c45ec98e1d9f4
Alek Dróżdż <Alek Drd>
2016-07-26 18:46:10 +0200

    Config and game title

--- core/src/com/mygdx/hanoi/HanoiGame.java
+++ core/src/com/mygdx/hanoi/HanoiGame.java
@@ -2,32 +2,38 @@ package com.mygdx.hanoi;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class HanoiGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
 	
-	@Override
-	public void create () {
+	public final static String GAME_NAME = "Towers of Hanoi";
+	private SpriteBatch batch;
+	private BitmapFont font;
+
+	
+	public void create() {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		font = new BitmapFont();
+		font.setColor(Color.BLACK);
+		font.getData().setScale(2.3f);
 	}
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+	
+	public void render() {
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		font.draw(batch, GAME_NAME, 280,440);
 		batch.end();
 	}
+
 	
-	@Override
-	public void dispose () {
+	public void dispose() {
 		batch.dispose();
-		img.dispose();
+		font.dispose();
+
 	}
 }
--- desktop/src/com/mygdx/hanoi/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/hanoi/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.hanoi.HanoiGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = HanoiGame.GAME_NAME;
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new HanoiGame(), config);
 	}
 }
